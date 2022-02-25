83676ff71499951b56f5551592f3216e51cd4a36
BinhNguyen2335 <100409700+BinhNguyen2335@users.noreply.github.com>
2022-02-25 16:21:43 +0700

    Add files via upload

---
+++ bai04-toantu/index.php
@@ -0,0 +1,18 @@
+<?php
+ $a = 5;
+ $b = 3;
+ $tong = $a + $b;
+ echo "Tong: $tong <br>";
+
+ $hieu = $a - $b;
+ echo "Hieu: $hieu <br>";
+
+ $tich = $a * $b;
+ echo "tich: $tich <br>";
+
+ $thuong = $a / $b;
+ echo "thuong: $thuong <br>";
+
+ $sodu = $a % $b;
+ echo "So du: $sodu <br>";
+?>  
